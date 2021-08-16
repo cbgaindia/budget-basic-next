282c354421775f1192a6c0e4507fa56331e5a8f6
PixeledCode <ashoaib540918@gmail.com>
2021-08-16 15:18:40 +0530

    created new footer data file

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,108 +1,5 @@
 import Image from 'next/image';
-
-const Dashboards = {
-  name: 'Dashboards',
-  links: [
-    {
-      title: 'Union Budget Explorer 2021-22',
-      value: 'https://union.openbudgetsindia.org/en/',
-    },
-    {
-      title: 'Union Budget Explorer 2020-21',
-      value: 'https://union2020.openbudgetsindia.org/en/',
-    },
-    {
-      title: 'Union Budget Explorer 2019-20',
-      value: 'https://union2019.openbudgetsindia.org/en/',
-    },
-    {
-      title: 'Union Budget Explorer 2019-20 (I)',
-      value: 'https://union2019i.openbudgetsindia.org/en/',
-    },
-    {
-      title: 'Union Budget Explorer 2018-19',
-      value: 'https://union2018.openbudgetsindia.org/en/',
-    },
-    {
-      title: 'Himachal Pradesh Fiscal Data Explorer',
-      value: 'https://hp.openbudgetsindia.org/',
-    },
-    {
-      title: 'Assam Budget Explorer 2020-21',
-      value: 'https://assam2020.openbudgetsindia.org/en/',
-    },
-    {
-      title: 'Assam Budget Explorer 2019-20',
-      value: 'https://assam2019.openbudgetsindia.org/en/',
-    },
-    {
-      title: 'Balasore District Treasury',
-      value:
-        'https://dash.openbudgetsindia.org/superset/dashboard/odisha_balasore_treasury_dashboard/?standalone=true',
-    },
-    {
-      title: 'Krishna District Treasury',
-      value:
-        'https://dash.openbudgetsindia.org/superset/dashboard/ap_krishna_treasury_dashboard/?standalone=true',
-    },
-    {
-      title: 'Schemes Dashboard',
-      value: 'https://schemes.openbudgetsindia.org',
-    },
-    {
-      title: 'Story Generator',
-      value: 'https://cbgaindia.github.io/story-generator',
-    },
-  ],
-};
-
-const Budget_Datasets = {
-  name: 'Budget Datasets',
-  links: [
-    {
-      title: 'Government-wise Budget Data',
-      value: 'https://openbudgetsindia.org/organization',
-    },
-    {
-      title: 'Sector-wise Budget Data',
-      value: 'https://openbudgetsindia.org/group',
-    },
-    {
-      title: 'All Datasets',
-      value: 'https://openbudgetsindia.org/dataset',
-    },
-  ],
-};
-
-const OBI_Platform = {
-  name: 'OBI Platform',
-  links: [
-    {
-      title: 'How to use the OBI Platform',
-      value: 'https://openbudgetsindia.org/pages/how-to-use-the-portal',
-    },
-    {
-      title: 'FAQs on the Platform',
-      value: 'https://openbudgetsindia.org/pages/faqs',
-    },
-    {
-      title: 'About OBI Platform',
-      value: 'https://openbudgetsindia.org/about',
-    },
-    {
-      title: 'Video: OBI Platform',
-      value: 'https://youtu.be/xKjzH1ZB3c4',
-    },
-    {
-      title: 'Video: Budget Basics',
-      value: 'https://youtu.be/fGxNh5Xfn2I',
-    },
-    {
-      title: 'Video: Budget Basics (Hindi)',
-      value: 'https://www.youtube.com/watch?v=TovrkaW5HZY',
-    },
-  ],
-};
+import * as data from './footer_data';
 
 const Footer = () => (
   <footer className="footer">
@@ -124,8 +21,8 @@ const Footer = () => (
 
       <div className="footer__links">
         <section className="footer__links__section">
-          <p>{Dashboards.name}</p>
-          {Dashboards.links.map((link) => (
+          <p>{data.Dashboards.name}</p>
+          {data.Dashboards.links.map((link) => (
             <a
               className="link footer_link"
               rel="noopener noreferrer"
@@ -138,8 +35,8 @@ const Footer = () => (
         </section>
 
         <section className="footer__links__section">
-          <p>{Budget_Datasets.name}</p>
-          {Budget_Datasets.links.map((link) => (
+          <p>{data.Budget_Datasets.name}</p>
+          {data.Budget_Datasets.links.map((link) => (
             <a
               className="link footer_link"
               rel="noopener noreferrer"
@@ -152,8 +49,8 @@ const Footer = () => (
         </section>
 
         <section className="footer__links__section">
-          <p>{OBI_Platform.name}</p>
-          {OBI_Platform.links.map((link) => (
+          <p>{data.OBI_Platform.name}</p>
+          {data.OBI_Platform.links.map((link) => (
             <a
               className="link footer_link"
               rel="noopener noreferrer"
@@ -170,107 +67,54 @@ const Footer = () => (
     <div className="footer__attr">
       <div className="footer__attr__wrapper footer__wrapper">
         <div className="footer__attr__links">
-          <a
-            rel="noopener noreferrer"
-            href="https://openbudgetsindia.org/pages/disclaimers"
-            target="_blank"
-            className="link footer_link"
-          >
-            Disclaimer
-          </a>
-          <a
-            rel="noopener noreferrer"
-            className="link footer_link"
-            href="https://openbudgetsindia.org/pages/license"
-            target="_blank"
-          >
-            License
-          </a>
-          <a
-            rel="noopener noreferrer"
-            className="link footer_link"
-            href="https://openbudgetsindia.org/contact"
-            target="_blank"
-          >
-            Contact Us
-          </a>
+          {data.Attr_Links.links.map((link) => (
+            <a
+              rel="noopener noreferrer"
+              href={link.value}
+              target="_blank"
+              className="link footer_link"
+            >
+              {link.title}
+            </a>
+          ))}
         </div>
 
         <div className="footer__attr__logos">
-          <a
-            rel="nofollow noopener noreferrer"
-            href="https://creativecommons.org/licenses/by/4.0/"
-            target="_blank"
-            className="link footer_link"
-          >
-            <Image
-              src="/assets/icons/cc-by-logo.svg"
-              alt="cc logo obi"
-              layout="fixed"
-              width={67}
-              height={23}
-            />
-          </a>
-          <a
-            rel="nofollow noopener noreferrer"
-            href="https://opendefinition.org/od/2.1/en/"
-            target="_blank"
-            className="link footer_link"
-          >
-            <Image
-              src="/assets/icons/open_data_logo.png"
-              alt="open data logo obi"
-              className="card-link-container"
-              layout="fixed"
-              width={80}
-              height={15}
-            />
-          </a>
+          {data.Attr_Logos.links.map((link) => (
+            <a
+              rel="nofollow noopener noreferrer"
+              href={link.value}
+              target="_blank"
+              className="link footer_link"
+            >
+              <Image
+                src={link.src}
+                alt={link.alt}
+                layout="fixed"
+                width={67}
+                height={23}
+              />
+            </a>
+          ))}
         </div>
 
         <div className="footer__attr__social">
-          <a
-            rel="nofollow noopener noreferrer"
-            className="link footer_link"
-            href="https://github.com/cbgaindia"
-            target="_blank"
-          >
-            <Image
-              src="/assets/icons/github-icon.svg"
-              alt="github logo obi"
-              layout="fixed"
-              width={23}
-              height={23}
-            />
-          </a>
-          <a
-            rel="nofollow noopener noreferrer"
-            className="link footer_link"
-            href="https://twitter.com/OpenBudgetsIn"
-            target="_blank"
-          >
-            <Image
-              src="/assets/icons/twitter-icon.svg"
-              alt="twitter logo obi"
-              layout="fixed"
-              width={23}
-              height={23}
-            />
-          </a>
-          <a
-            rel="nofollow noopener noreferrer"
-            className="link footer_link"
-            href="https://www.facebook.com/OpenBudgetsIndia"
-            target="_blank"
-          >
-            <Image
-              src="/assets/icons/facebook-icon.svg"
-              alt="facebook logo obi"
-              layout="fixed"
-              width={23}
-              height={23}
-            />
-          </a>
+          {data.Attr_Social.links.map((link) => (
+            <a
+              rel="nofollow noopener noreferrer"
+              href={link.value}
+              target="_blank"
+              className="link footer_link"
+            >
+              <Image
+                src={link.src}
+                alt={link.alt}
+                layout="fixed"
+                width={23}
+                height={23}
+              />
+            </a>
+          ))}
         </div>
       </div>
     </div>
---
+++ components/footer/footer_data.js
@@ -0,0 +1,155 @@
+export const Dashboards = {
+  name: 'Dashboards',
+  links: [
+    {
+      title: 'Union Budget Explorer 2021-22',
+      value: 'https://union.openbudgetsindia.org/en/',
+    },
+    {
+      title: 'Union Budget Explorer 2020-21',
+      value: 'https://union2020.openbudgetsindia.org/en/',
+    },
+    {
+      title: 'Union Budget Explorer 2019-20',
+      value: 'https://union2019.openbudgetsindia.org/en/',
+    },
+    {
+      title: 'Union Budget Explorer 2019-20 (I)',
+      value: 'https://union2019i.openbudgetsindia.org/en/',
+    },
+    {
+      title: 'Union Budget Explorer 2018-19',
+      value: 'https://union2018.openbudgetsindia.org/en/',
+    },
+    {
+      title: 'Himachal Pradesh Fiscal Data Explorer',
+      value: 'https://hp.openbudgetsindia.org/',
+    },
+    {
+      title: 'Assam Budget Explorer 2020-21',
+      value: 'https://assam2020.openbudgetsindia.org/en/',
+    },
+    {
+      title: 'Assam Budget Explorer 2019-20',
+      value: 'https://assam2019.openbudgetsindia.org/en/',
+    },
+    {
+      title: 'Balasore District Treasury',
+      value:
+        'https://dash.openbudgetsindia.org/superset/dashboard/odisha_balasore_treasury_dashboard/?standalone=true',
+    },
+    {
+      title: 'Krishna District Treasury',
+      value:
+        'https://dash.openbudgetsindia.org/superset/dashboard/ap_krishna_treasury_dashboard/?standalone=true',
+    },
+    {
+      title: 'Schemes Dashboard',
+      value: 'https://schemes.openbudgetsindia.org',
+    },
+    {
+      title: 'Story Generator',
+      value: 'https://cbgaindia.github.io/story-generator',
+    },
+  ],
+};
+
+export const Budget_Datasets = {
+  name: 'Budget Datasets',
+  links: [
+    {
+      title: 'Government-wise Budget Data',
+      value: 'https://openbudgetsindia.org/organization',
+    },
+    {
+      title: 'Sector-wise Budget Data',
+      value: 'https://openbudgetsindia.org/group',
+    },
+    {
+      title: 'All Datasets',
+      value: 'https://openbudgetsindia.org/dataset',
+    },
+  ],
+};
+
+export const OBI_Platform = {
+  name: 'OBI Platform',
+  links: [
+    {
+      title: 'How to use the OBI Platform',
+      value: 'https://openbudgetsindia.org/pages/how-to-use-the-portal',
+    },
+    {
+      title: 'FAQs on the Platform',
+      value: 'https://openbudgetsindia.org/pages/faqs',
+    },
+    {
+      title: 'About OBI Platform',
+      value: 'https://openbudgetsindia.org/about',
+    },
+    {
+      title: 'Video: OBI Platform',
+      value: 'https://youtu.be/xKjzH1ZB3c4',
+    },
+    {
+      title: 'Video: Budget Basics',
+      value: 'https://youtu.be/fGxNh5Xfn2I',
+    },
+    {
+      title: 'Video: Budget Basics (Hindi)',
+      value: 'https://www.youtube.com/watch?v=TovrkaW5HZY',
+    },
+  ],
+};
+
+export const Attr_Links = {
+  links: [
+    {
+      title: 'Disclaimer',
+      value: 'https://openbudgetsindia.org/pages/disclaimers',
+    },
+    {
+      title: 'License',
+      value: 'https://openbudgetsindia.org/pages/license',
+    },
+    {
+      title: 'Contact Us',
+      value: 'https://openbudgetsindia.org/contact',
+    },
+  ],
+};
+
+export const Attr_Logos = {
+  links: [
+    {
+      alt: 'cc logo obi',
+      src: '/assets/icons/cc-by-logo.svg',
+      value: 'https://creativecommons.org/licenses/by/4.0/',
+    },
+    {
+      alt: 'open data logo obi',
+      src: '/assets/icons/open_data_logo.png',
+      value: 'https://opendefinition.org/od/2.1/en/',
+    },
+  ],
+};
+
+export const Attr_Social = {
+  links: [
+    {
+      alt: 'github logo obi',
+      src: '/assets/icons/github-icon.svg',
+      value: 'https://github.com/cbgaindia',
+    },
+    {
+      alt: 'twitter logo obi',
+      src: '/assets/icons/twitter-icon.svg',
+      value: 'https://twitter.com/OpenBudgetsIn',
+    },
+    {
+      alt: 'facebook logo obi',
+      src: '/assets/icons/facebook-icon.svg',
+      value: 'https://www.facebook.com/OpenBudgetsIndia',
+    },
+  ],
+};
