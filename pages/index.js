7ebbb9ad348977a0913bdd2b4e41b4ba9ba18309
rajan-kiwi <rajan.kumar@kiwitech.com>
2021-12-11 22:34:05 +0530

    chages made

--- pages/index.js
+++ pages/index.js
@@ -10,27 +10,50 @@ import Carousel from 'components/carousel/carousel';
 
 export default function Home({ homepage, chapters }) {
   sortList(chapters);
-  function headerDesc() {
-    return <p className="header__desc">{homepage.heading}</p>;
+  
+  function HeaderDesc() {
+    return (
+      <div className="homepage_heading_new_container">
+         <h1 className="homepage_heading_new">{homepage.heading}</h1>
+      </div>
+    )
   }
+
+  function romanizeNumber (num) {
+    if (isNaN(num))
+        return NaN;
+    var digits = String(+num).split(""),
+        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+               "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+    return Array(+digits.join("") + 1).join("M") + roman;
+}
 
   return (
     <>
       <Seo seo={homepage.seo} />
 
-      <Header desc={headerDesc()} color="#101524" />
-      {homepage.highlight.length > 0 && <Highlight data={homepage.highlight} />}
+      <Header color="#212142" />
+      {/* {homepage.highlight.length > 0 && <Highlight data={homepage.highlight} />} */}
 
       <section className="home__mobile-search">
         <Link href="/search">
           <a>Search</a>
         </Link>
       </section>
+      <section className="homepage_heading_new_sec">
+        <HeaderDesc />
+      </section>
+      
       <div className="skiptarget">
         <span id="maincontent">-</span>
       </div>
-      <main id="main" tabIndex="-1" className="wrapper">
-        <ul className="home__cards">
+      <main id="main" tabIndex="-1" className="wrapper no_focus">
+        <ul className="new_home__cards">
           {chapters.map((chapter, index) => {
             const chapterDetails = {
               title: chapter.Title,
@@ -39,6 +62,7 @@ export default function Home({ homepage, chapters }) {
               Desc: chapter.Desc,
               totalArticles: chapter.sections.length,
               index,
+              chapterNumber:romanizeNumber(chapter.Chapter_No)
             };
             return (
               <React.Fragment key={index}>
