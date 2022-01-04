9b6114181baedf6adf704d31ee607cb380f1349b
Rajan kumar <48202870+Rajankr542@users.noreply.github.com>
2022-01-04 18:11:40 +0530

    updated codes as per remarks

--- pages/_app.js
+++ pages/_app.js
@@ -56,6 +56,9 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
