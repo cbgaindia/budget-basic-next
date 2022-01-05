a5f178be6008f9c9dd90b10549cd12027bc398db
Rajan kumar <48202870+Rajankr542@users.noreply.github.com>
2022-01-05 21:58:06 +0530

    Updated the codes

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -81,7 +81,7 @@ const Navigation = ({ back, forward }) => (
                   <img className="image_pre_next_navigation" src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${forward.icon.url}`} alt="" />
                 </div>
                 <span>
-                  <p>next Chapter</p>
+                  <p>Next Chapter</p>
                   <h2>{forward.Title}</h2>
                 </span>
               </a>
